c20554b9661e5cfe3f362dfe4f93580b07b9014d
Justin Ellis <jdellis490@gmail.com>
2022-05-04 19:25:09 -0700

    index.js functions added

---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown')
+const fs = require('fs');
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of the project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe the project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How is the project installed?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How is the project used?',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'How do I contribute to this project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'How is the project tested?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license was used?',
+      choices: '[MIT, GPLv2, GPLv3, Other]'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?',
+    },
+];
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const markdownInfo = generateMarkdown(data);
+// TODO: Create a function to write README file
+        fs.writeFile('README.md', markdownInfo, (err) => {
+            if (err){
+                console.log(err);
+            }else console.log('README generated!');
+        })
+    })
+};
+
+
+
+
+// Function call to initialize app
+init();
